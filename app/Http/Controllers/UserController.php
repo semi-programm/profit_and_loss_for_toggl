bbedebd1cb2d87ef17b8d43c9b833246cc666b76
utsugi <57470635+utsugi32@users.noreply.github.com>
2020-01-31 18:10:56 +0900

    user 途中

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,32 +2,71 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use App\Model\User;
-use Carbon\Carbon;
+use Illuminate\Support\Carbon;
+use Yasumi\Yasumi;
 
-class Usercontroller extends Controller
+class UserController extends Controller
 {
-    public function index ()
+    public function index()
     {
         $users = User::all();
 
-        $this_month = Carbon::now()->month;
+        // $this_month = Carbon::now()->month;
 
-        $users->each(function($user) use ($this_month){
-            $times = $user->timeEntries()->whereMonth('start', '=' ,$this_month)->get();
-            $sum_time = $times->sum('duration'); //単位はms
-            $user['sum_time'] = $sum_time/(1000*60*60);
-        });
+        // $users->each(function($user) use ($this_month){
+        //     $times = $user->timeEntries()->whereMonth('start', '=' ,$this_month)->get();
+        //     $sum_time = $times->sum('duration'); //単位はms
+        //     $user['sum_time'] = $sum_time/(1000*60*60);
+        // });
 
+        $startDate = Carbon::now();
+        $endDate = Carbon::now()->endOfMonth();
+        $weekdays = $this->Weekdays($startDate, $endDate);
+        dump($weekdays);
+        $year = Carbon::now()->year()->toDateString();
 
-
-        return view ('user.index', ['users' => $users]);
+        // return view ('user.index', ['users' => $users]);
     }
 
-    public function view ()
+    public function view()
     {
         $users = User::all();
         return view('user.view', $users);
+    }
+
+    public function Weekdays($startDate, $endDate)
+    {
+        $start_date_p = $startDate;
+        $year = $startDate->year()->toDateString();
+        // 土日を除く平日を取得
+        $days = (int) $start_date_p->diffInDaysFiltered(
+            function (Carbon $date) {
+                return $date->isWeekday();
+            },
+            $endDate
+        );
+
+        // 祝日を取得
+        $holidays = Yasumi::create('Japan', $year, 'ja_JP');
+
+        $holidaysInBetweenDays = $holidays->between(
+            \DateTime::createFromFormat('m/d/Y', $startDate->format('m/d/Y')),
+            \DateTime::createFromFormat('m/d/Y', $endDate->format('m/d/Y'))
+        );
+
+        $numberOfHoliday = 0;
+        foreach ($holidaysInBetweenDays as $holiday) {
+            if ((new Carbon($holiday))->isWeekend() === false) {
+                $numberOfHoliday++;
+            }
+        }
+
+        // さらに祝日の数を引いた平日の日数を取得
+        $numberOfDay = $days - $numberOfHoliday;
+
+        // 出力
+        return $numberOfDay;
     }
 }
