78878b388369dce3db56c7baeaa64b235a54e5db
utsugi <57470635+utsugi32@users.noreply.github.com>
2020-02-01 13:26:02 +0900

    Fix command

--- app/Console/Commands/GetTogglAllData.php
+++ app/Console/Commands/GetTogglAllData.php
@@ -38,14 +38,16 @@ class GetTogglAllData extends Command
      */
     public function handle()
     {
+        $since = $this->argument('since');
+        $until = $this->argument('until');
         // TODO:workspaceが無いと、他のAPIでgetができないので、処理が終わったか判定しないとならない。
         // TODO:foreign keyも加味して、clientsの後にprojectsをgetしないと、DBのエラーが出る。
-            $this->call('toggl:workspaces');
-            $this->call('toggl:users');
-            $this->call('toggl:clients');
-            $this->call('toggl:projects');
-            $this->call('toggl:tags');
-            $this->call('toggl:tasks');
-            $this->call('toggl:entries{since}{until?}');
+        $this->call('toggl:workspaces');
+        $this->call('toggl:users');
+        $this->call('toggl:clients');
+        $this->call('toggl:projects');
+        $this->call('toggl:tags');
+        $this->call('toggl:tasks');
+        $this->call('toggl:entries', ['since' => $since, 'until' => $until ?? null]);
     }
 }
