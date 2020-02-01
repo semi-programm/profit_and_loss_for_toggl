12f2c05caee279395b0ca1971f9b0d6b26659ddc
utsugi <57470635+utsugi32@users.noreply.github.com>
2020-02-01 13:19:39 +0900

    Add command 全コマンドを叩く

---
+++ app/Console/Commands/GetTogglAllData.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+
+class GetTogglAllData extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'toggl:all{since}{until?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'togglの全データをgetしてDBにinsertする';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // TODO:workspaceが無いと、他のAPIでgetができないので、処理が終わったか判定しないとならない。
+        // TODO:foreign keyも加味して、clientsの後にprojectsをgetしないと、DBのエラーが出る。
+            $this->call('toggl:workspaces');
+            $this->call('toggl:users');
+            $this->call('toggl:clients');
+            $this->call('toggl:projects');
+            $this->call('toggl:tags');
+            $this->call('toggl:tasks');
+            $this->call('toggl:entries{since}{until?}');
+    }
+}
